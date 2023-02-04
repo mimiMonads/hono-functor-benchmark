d015975db825b80e355cae42ebde69c3ce6c5d58
Mimi <122406442+mimiMonads@users.noreply.github.com>
2023-02-04 00:07:10 +0000

    Update main.ts

--- hono/main.ts
+++ hono/main.ts
@@ -4,32 +4,38 @@ import { Hono } from "https://deno.land/x/hono/mod.ts";
 const app = new Hono();
 
 app.get("/", (c) => c.text("hello world"));
-app.get("/a/b/c/d/e/f/g/", (c) => {
-  return c.text("hello world");
-});
-app.get("/test/:id", (c) => {
-  return c.text(c.req.param("id"));
-});
 
-app.get("/test", (c) => {
-  return c.text(c.req.query("hello") || "");
+app.get("/a/b/c/d/e/f/g/", (c) => {
+  return c.text("hello world2");
 });
-app.get("/test/both/:id2", (c) => {
-  return c.text(c.req.param("id2") + " " + (c.req.query("hello") || ""));
+app.get("/test/:u", (c) => {
+  return c.text(c.req.param("u"));
 });
 
 app.get("/test/mul/:a/:b/:c", (c) => {
-  return c.text(c.req.param("b"));
+  return c.text(c.req.param("a") + c.req.param("b") +c.req.param("c"));
 });
 
 app.get("/q", (c) => {
-  return c.text(c.req.query().e || "");
+  return c.text(c.req.query("d") || "");
 });
-app.get("/json", async (c) => {
-  return c.text(await Deno.readTextFile("./people.json"));
+
+app.get("/multi", (c) => {
+  return c.text((c.req.query("d") || "") + (c.req.query("e") || "") + (c.req.query("f") || ""));
 });
+
+app.get("/test/both/:a", (c) => {
+  return c.text(c.req.param("a") + " " + (c.req.query("d") || ""));
+});
+
+app.get("/both/test/:q/:w/:e", (c) => {
+  return c.text(c.req.param("q") + c.req.param("w") +c.req.param("e") + (c.req.query("d") || "") + (c.req.query("e") || "") + (c.req.query("f") || "") + c.req.queries("a"));
+});
+
+
 
 await serve((req) => app.fetch(req), {
   port: 8080,
   hostname: "127.0.0.1",
 });
+
